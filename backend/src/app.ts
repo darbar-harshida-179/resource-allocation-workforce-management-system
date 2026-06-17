18429daa3158399b6dbea3541831e8436b1b08c2
darbar-harshida-179 <darbarharshida179@gmail.com>
2026-06-17 14:55:04 +0530

    Fix production CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,6 +20,7 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
+     "https://resource-allocation-workforce-management-system-bql53viar.vercel.app",
     process.env.FRONTEND_URL || "http://localhost:3000"
   ],
   credentials: true,
